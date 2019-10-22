ad41bad435b32f77e4bbb34f1421c1015ff91c18
PiotrStudio <56451021+PiotrStudio@users.noreply.github.com>
2019-10-22 18:43:23 +0200

    1

--- action/add_reaction_post.php
+++ action/add_reaction_post.php
@@ -1,7 +1,18 @@
 <?php
 $error = 1;
 
-if(isset($_REQUEST['post_id']) && isset($_REQUEST['']))
+if(isset($_REQUEST['post_id']) && is_numeric($_REQUEST['post_id'])
+&& isset($_REQUEST['reaction']) && $_REQUEST['reaction'] == 1 or $_REQUEST['reaction'] == 0 or $_REQUEST['reaction'] == -1)
+{
+    $post_id = $_REQUEST['post_id'];
+    $reaction = $_REQUEST['reaction'];
+    $post = new Post();
+
+    if($post -> resultConnection)
+    {
+        $post -> getOnlyPost($);
+    }
+}
 
 header('Location:index.php?error='.$error);
 exit;
