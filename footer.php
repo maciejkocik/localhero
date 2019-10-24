8603e9ed8467c3dc6f17469fc202954cfa4f7393
maciejkocik <44751007+maciejkocik@users.noreply.github.com>
2019-10-24 21:54:23 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -13,16 +13,13 @@ $(function() {
       $file_upload = $('#image_upload', $form), 
       $button = $('.submit', $form); 
 
-  $button.prop('disabled', 'disabled');
+
 
   $file_upload.on('change', function () {
     var number_of_images = $(this)[0].files.length;
     if (number_of_images > max_file_number) {
       alert(`Możesz dodać maksymalnie ${max_file_number} zdjęć.`);
       $(this).val('');
-      $button.prop('disabled', 'disabled');
-    } else {
-      $button.prop('disabled', false);
     }
   });
 });
