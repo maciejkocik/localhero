2256f06f9cc0ca098d86260e918a00aeca619dda
maciejkocik <44751007+maciejkocik@users.noreply.github.com>
2019-10-25 02:44:27 +0200

    added marker display on main page

--- footer.php
+++ footer.php
@@ -13,15 +13,35 @@
             center: new google.maps.LatLng(51.9358379,16.8921266),
             zoom: 5
         }
-     
+  //map 1 - main_page   
         <?php if(!isset($_GET['page']) || $_GET['page'] == "add_post" || $_GET['page'] == "main_page"){ ?>
         map = new google.maps.Map(document.getElementById("map"), myOptions);
-        <?php } ?>
         
+        function addMarker(lat, lng){
+        var pt = new google.maps.LatLng(lat, lng);
+        var marker = new google.maps.Marker({
+            position: pt,
+            map: map
+        });
+        }
+        
+        <?php if(isset($post)) {
+                    $post -> getApprovedPostsForMap();
+                    $approved_posts = $post -> approvedPosts;
+                    for ($i = 0; $i < count($approved_posts); $i++){
+                        echo "addMarker(".$approved_posts[$i]['lat'].",".$approved_posts[$i]['lng'].");";
+                        //echo "console.log(".$approved_posts[$i]['lat'].");";
+                    }
+                }
+        }
+        ?>
+  
+  //map 2 - add_post
         <?php if($signed_in){ ?>
         map2 = new google.maps.Map(document.getElementById("map2"), myOptions);
         <?php } ?>
-        
+  
+  //map 3 - add_cleaned_up
         <?php if(isset($_GET['page']) && ($_GET['page'] == "view_post" || $_GET['page'] == 'add_cleaned_up') && isset($post)){
     
         echo 'var postLocation = {
@@ -41,7 +61,8 @@
             });
         
         <?php } ?>
-        
+   
+  //getting user location
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
             var userLocation = {
@@ -52,7 +73,7 @@
             map2.setZoom(10);
           });
        }
-        
+  //marker for map2      
         google.maps.event.addListener(map2, 'click', function(event) {
             placeMarker(event.latLng, map2);
         });
@@ -77,7 +98,8 @@
 
     </script>
 
-        <script>
+    <script>
+    //script for adding photos
 $(function() {
 
   var
